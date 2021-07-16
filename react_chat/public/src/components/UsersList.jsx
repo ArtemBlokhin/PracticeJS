f4eabd7f541cf264c7ed33ba4c0709fd0a0df3d1
ArtemBlokhin <75878060+ArtemBlokhin@users.noreply.github.com>
2021-07-16 15:19:45 +0300

    React_chat v.2

---
+++ react_chat/public/src/components/UsersList.jsx
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+'use strict';
+var UsersList = React.createClass({
+    render:function() {
+        var users = this.props.users.map(function(){
+            return <div className='chat-user'>{user}</div>
+        })
+        return (
+            <div className='col-xs-3 col-md-4 col-lg-4'>
+                {users}
+            </div>
+        )
+    }
+})
